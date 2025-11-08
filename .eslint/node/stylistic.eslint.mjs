68853a476c2da0cd96ecadca4affa6ccac98aaa4
Dmytro Vakulenko <drsmile444@users.noreply.github.com>
2025-11-08 22:32:41 +0200

    feat: update ESLint configuration for improved blank line rules and version bump to 1.3.0

--- .eslint/node/stylistic.eslint.mjs
+++ .eslint/node/stylistic.eslint.mjs
@@ -3,7 +3,7 @@ import stylistic from '@stylistic/eslint-plugin';
 /**
  * @description ESLint config for enforcing stylistic code rules using @stylistic/eslint-plugin. Applies padding and formatting rules for code consistency.
  * @author Dmytro Vakulenko
- * @version 1.2
+ * @version 1.3.0
  * @see https://github.com/eslint-stylistic/eslint-stylistic
  */
 export default [
@@ -51,17 +51,28 @@ export default [
         },
         {
           blankLine: 'always',
-          next: ['export', 'class', 'default', 'function'],
-          prev: ['export', 'class', 'default', 'function'],
+          next: ['class', 'function'],
+          prev: ['class', 'function'],
         },
+        // Note: multiline-const/let/expression rules do NOT apply to exports (handled separately below)
         {
           blankLine: 'always',
           next: ['multiline-const', 'multiline-let', 'multiline-expression'],
           prev: '*',
         },
         {
+          blankLine: 'any',
+          next: ['multiline-const', 'multiline-let', 'multiline-expression'],
+          prev: 'export',
+        },
+        {
           blankLine: 'always',
           next: '*',
+          prev: ['multiline-const', 'multiline-let', 'multiline-expression'],
+        },
+        {
+          blankLine: 'any',
+          next: 'export',
           prev: ['multiline-const', 'multiline-let', 'multiline-expression'],
         },
         { blankLine: 'always', next: 'block', prev: '*' },
@@ -69,6 +80,27 @@ export default [
         { blankLine: 'always', next: 'block-like', prev: '*' },
         { blankLine: 'always', next: '*', prev: 'block-like' },
         { blankLine: 'always', next: 'return', prev: '*' },
+        // Allow flexible spacing for exports (to accommodate JSDoc comments) - must come before case/default rules
+        {
+          blankLine: 'any',
+          next: 'export',
+          prev: '*',
+        },
+        {
+          blankLine: 'any',
+          next: '*',
+          prev: 'export',
+        },
+        {
+          blankLine: 'any',
+          next: ['multiline-const', 'multiline-let', 'multiline-expression'],
+          prev: 'export',
+        },
+        {
+          blankLine: 'any',
+          next: 'export',
+          prev: ['multiline-const', 'multiline-let', 'multiline-expression'],
+        },
         // case/default rules - must come LAST to override all other rules including block-like
         // First, set default behavior: no blank lines between case/default statements
         { blankLine: 'never', next: 'default', prev: 'case' },
@@ -80,6 +112,19 @@ export default [
         // Then, override: always blank line after case block (with body) before next case/default
         { blankLine: 'always', next: 'case', prev: 'block-like' },
         { blankLine: 'always', next: 'default', prev: 'block-like' },
+      ],
+      // Enforce blank line before block comments (JSDoc), but not after
+      '@stylistic/lines-around-comment': [
+        'error',
+        {
+          beforeBlockComment: true,
+          afterBlockComment: false,
+          allowBlockStart: true,
+          allowObjectStart: true,
+          allowArrayStart: true,
+          allowClassStart: true,
+          applyDefaultIgnorePatterns: true,
+        },
       ],
     },
   },
